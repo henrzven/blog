c314c228a20bfc8833ab3c25877a43990e564603
David Jaimes <weritovenezuela@gmail.com>
2022-05-13 15:28:35 -0400

    commit message

---
+++ app/javascript/application.js
@@ -0,0 +1,16 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import * as bootstrap from "bootstrap"
+
+import 'bootstrap/js/src/alert'
+import 'bootstrap/js/src/button'
+import 'bootstrap/js/src/carousel'
+import 'bootstrap/js/src/collapse'
+import 'bootstrap/js/src/dropdown'
+import 'bootstrap/js/src/modal'
+import 'bootstrap/js/src/popover'
+import 'bootstrap/js/src/scrollspy'
+import 'bootstrap/js/src/tab'
+import 'bootstrap/js/src/toast'
+import 'bootstrap/js/src/tooltip'
